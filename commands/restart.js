d76be9c9bb4fcbd97a4b4871a6fea296200adfab
Jayson <32702660+jaysonhutchison@users.noreply.github.com>
2019-09-04 13:25:05 -0500

    Added pm2

--- commands/restart.js
+++ commands/restart.js
@@ -1,5 +1,14 @@
+const cmd = require('node-cmd');
+const fs = require('fs');
+let id = require('../restart.json');
 module.exports = (client, message, args) => {
-    message.channel.send('Bot restarting...');
-    client.destroy();
-    process.exit(1);
+    message.channel.send("Restarting...");
+    id.id = message.channel.id;
+    fs.writeFile('restart.json', JSON.stringify(id), err => console.error);
+    cmd.get(
+        'pm2 restart main',
+        function(err, data, stderr){
+            message.channel.send(err.message);
+        }
+    );
 };
--- main.js
+++ main.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const apiaiApp = require('apiai')(process.env.AI_TOKEN);
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const id = require('./restart.json');
 client.on('ready', function (evt) {
     console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
     client.user.setActivity('Female Replacement Intelligent Digital Assistant Youth');
+    // client.channels[id.id].send("Restarted!");
+    client.channels.find(x => x.id === id.id).send('Restarted!');
 });
 
 const timediff = require('timediff');
@@ -36,7 +39,6 @@ client.on('message', message => {
     for (const i of prefixes) {
         if (message.content.toLowerCase().startsWith(i)) prefix = i;
     }
-    if (message.content.toLowerCase().indexOf(prefix) !== 0 && message.channel.type !== 'dm') return;
     if (prefix == process.env.PREFIX) {
         if (message.author.id === '359988404316012547') {
             try {
@@ -50,6 +52,7 @@ client.on('message', message => {
         }
         return;
     }
+    if (message.content.toLowerCase().indexOf(prefix) !== 0 && message.channel.type !== 'dm') return;
     message.channel.startTyping();
 try {
     let text = message.content.substring(prefix.length);
@@ -71,7 +74,7 @@ try {
                 }
             };
             a('date.between', function() {return timediff(args.date1, args.date2, args.unit[0].toUpperCase())[args.unit+'s']+` ${args.unit}s`});
-            a(['date.check', 'date.check - context:date-check'], function() {
+            a(['date.check', 'date.check - context:date-check', 'date.day_of_week'], function() {
                 const location = typeof args.location == 'string' ? args.location : args.location.country;
                 const { timezone, time_diff } = getTimeDiffAndTimeZone(location);
                 let time = new Date();
