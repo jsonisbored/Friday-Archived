c2f66867f671a1ac100cfcea924edcba4c4d8356
Jayson <32702660+jaysonhutchison@users.noreply.github.com>
2019-09-04 19:04:10 -0500

    Added holidays

--- commands/restart.js
+++ commands/restart.js
@@ -1,12 +1,12 @@
 const cmd = require('node-cmd');
 const fs = require('fs');
 let id = require('../restart.json');
-module.exports = (client, message, args) => {
-    message.channel.send("Restarting...");
+module.exports = async (client, message, args) => {
+    await message.channel.send("Restarting...");
     id.id = message.channel.id;
     fs.writeFile('restart.json', JSON.stringify(id), err => console.error);
     cmd.get(
-        'pm2 restart main',
+        'pm2 restart index',
         function(err, data, stderr){
             message.channel.send(err.message);
         }
--- index.js
+++ index.js
@@ -10,27 +10,49 @@ client.on('ready', function (evt) {
     else client.channels.find(x => x.id === id.id).send('Restarted!');
 });
 
+
 const timediff = require('timediff');
 const tz = require('timezone-id');
 const { listTimeZones, findTimeZone, getZonedTime, getUnixTime } = require('timezone-support');
 
-
 const formatDate = function(d) {
-    const minutes = d.minutes.toString().length == 1 ? '0'+d.minutes : d.minutes,
-    hours = d.hours.toString().length == 1 ? '0'+d.hours : d.hours,
-    ampm = d.hours >= 12 ? 'PM' : 'AM',
+    const minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'PM' : 'AM',
     months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
-    days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Fridar','Saturday'];
     return {
-        day: days[d.day],
-        month: months[d.month],
-        date: d.dayOfWeek,
-        year: d.year,
-        hours: hours%12,
+        day: days[d.getDay()],
+        month: months[d.getMonth()],
+        date: d.getDate(),
+        year: d.getFullYear(),
+        hours: hours%12 == 0 ? 12 : hours%12,
         minutes: minutes,
         ampm: ampm
     };
 };
+const holidays = { // Values are formatted as month,week,day,*date
+    "New Year": [0,0,0],
+    "Martin Luther King": [0,2,1],
+    "Valentine's Day": [1,0,0,13],
+    "Washington's Birthday": [1,2,1],
+    "Saint Patrick's Day": [2,0,0,16],
+    "Fools' Day": [3,0,0],
+    "Mother's Day": [4,1,0],
+    "Memorial Day": [4,-1,0],
+    "Father's Day": [5,2,0],
+    "Independence Day": [6,0,0,3],
+    "Labor Day": [8,0,1],
+    "Halloween": [9,0,0,30],
+    "Veterans Day": [10,0,0,10],
+    "Thanksgiving day": [10,3,4],
+    "Black Friday": [10,4,5],
+    "Easter": [10,4,7],
+    "Christmas Eve": [11,0,0,23],
+    "Christmas": [11,0,0,24],
+    "New Year's Eve": [11,-1,6],
+};
+
 
 let contexts = [];
 client.on('message', message => {
@@ -55,7 +77,7 @@ client.on('message', message => {
     }
     if (message.content.toLowerCase().indexOf(prefix) !== 0 && message.channel.type !== 'dm') return;
     message.channel.startTyping();
-try {
+    
     let text = message.content.substring(prefix.length);
     let request = apiaiApp.textRequest(text, {
         sessionId: message.author.id,
@@ -69,18 +91,20 @@ try {
                 if (typeof q != 'object') q = [q];
                 for (let i = 0; i < q.length; i ++) {
                     if (response.result.metadata.intentName == q[i]) {
-                        res = typeof value == 'function' ? value() : value;
+                        res = typeof value == 'function' ? value(q[i]) : value;
                     }
                 }
             };
             a('date.between', function() {return timediff(args.date1, args.date2, args.unit[0].toUpperCase())[args.unit+'s']+` ${args.unit}s`});
-            a(['date.check', 'date.check - context:date-check', 'date.day_of_week'], async function() {
-                const location = typeof args.location == 'string' ? args.location : args.location.country || args.location.city;
+            a(['date.check', 'date.check - context:date-check', 'date.day_of_week', 'date.day_of_week.check', 'date.get', 'date.get - context:date-get'], async function(q) {
+                const location = typeof args.location != 'object' ? (args.location || 'Chicago') : (args.location.country || args.location.city || 'Chicago');
                 const id = await tz.getTimeZone(location);
                 const zone = findTimeZone(id);
-                const t = formatDate(getZonedTime(new Date(), zone));
+                const d = getZonedTime(new Date(), zone);
+                const t = formatDate(new Date(d.year, d.month, d.day));
+                contexts = [];
                 contexts.push({
-                    name: 'date.check',
+                    name: q,
                     parameters: args
                 });
                 return {embed: {
@@ -89,6 +113,38 @@ try {
                     fields: [{
                         name: `${t.hours}:${t.minutes} ${t.ampm}`,
                         value: `${t.day}, ${t.month} ${t.date}, ${t.year}`
+                    }]
+                }};
+            });
+            a('date.holiday', function() {
+                let holiday = holidays[args.holiday],
+                firstDay = 1,
+                year = new Date(args.date.startDate).getFullYear() || new Date().getFullYear(),
+                month = holiday[0],
+                week = holiday[1],
+                day = holiday[2],
+                d = holiday[3]
+                t = formatDate(new Date());
+                if (d) {
+                    t = formatDate(new Date(year, month, d+1));
+                }  else {
+                    if (week < 0) {
+                        month++;
+                        firstDay--;
+                    }
+                    let date = new Date(year, month, (week * 7) + firstDay);
+                    if (day < date.getDay()) {
+                        day += 7;
+                    }
+                    date.setDate(date.getDate() - date.getDay() + day);
+                    t = formatDate(date);
+                }
+                return {embed: {
+                    color: 0xff6300,
+                    // title: args.holiday,
+                    fields: [{
+                        name: `${t.day}, ${t.month} ${t.date}`,
+                        value: `${args.holiday} ${t.year}`
                     }]
                 }};
             });
@@ -101,10 +157,6 @@ try {
         else message.channel.send("Oops, there was an error on our end.");
     });
     request.end();
-} catch(e) {
-    if (message.author.id === "359988404316012547") message.reply(e.message);
-    else message.channel.send("Oops, there was an error on our end.");
-}
 });
 
 client.login(process.env.DISCORD_TOKEN);
